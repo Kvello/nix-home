5329710cffcb5e9ae5bc41978daf62bb931f86c2
Kvello <markus.kv1@gmail.com>
2024-09-12 11:33:13 +0200

    Adds periodic pulling of logseq

--- scripts/logseq-sync-service.sh
+++ scripts/logseq-sync-service.sh
@@ -11,25 +11,30 @@ if [ ! -d "$LOGSEQ_DIR" ]; then
 fi
 # Navigate to the Logseq directory
 cd "$LOGSEQ_DIR"
+git config pull.rebase true
 git pull origin main
-# Monitor the directory for changes
-inotifywait -m -e  modify,create,delete,move -r . --exclude '(\.git)' --format '%w %e'|
-while read -r file event; do
-    # Print the event details (optional)
-    echo "Detected event: $event on $file"
-    # Pull any changes from the remote repository
-    git pull origin main
+while true; do
+	# Monitor the directory for changes
+	inotifywait -m -e  modify,create,delete,move -r . --exclude '(\.git)' --format '%w %e' --timeout 10 |
+	while read -r file event; do
+	    # Print the event details (optional)
+	    echo "Detected event: $event on $file"
+	    # Pull any changes from the remote repository
+	    git pull origin main
 
-    # Add any new or modified files to the staging area
-    git add .
+	    # Add any new or modified files to the staging area
+	    git add .
 
-    # Check if there are any changes to commit
-    if ! git diff --cached --quiet; then
-        # Commit the changes with a message if there are any
-        git commit -m "Automated sync"
+	    # Check if there are any changes to commit
+	    if ! git diff --cached --quiet; then
+		# Commit the changes with a message if there are any
+		git commit -m "Automated sync"
 
-        # Push the changes to the remote repository
-        git push origin main
-    fi
-    sleep 30
+		# Push the changes to the remote repository
+		git push origin main
+	    fi
+	    sleep 30
+	done
+
+git pull origin main
 done
