1a276bc93c4765c31bac4a2b464817d5372b6af4
Kvello <markus.kv1@gmail.com>
2024-10-08 16:17:21 +0200

    Adds keyboard layout(attempts)

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -21,7 +21,7 @@ in
   # manage.
   home.username = "markus";
   home.homeDirectory = "/home/markus";
-
+  home.keyboard.layout = "no";
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
